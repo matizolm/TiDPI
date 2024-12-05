644bc884e1ae7bdcb2bd9e9404252d5a04be0923
matizolm <145062076+matizolm@users.noreply.github.com>
2024-12-05 07:46:08 +0100

    Add files via upload

---
+++ Testowanieaplikacji/zad1 sortowanie babelkowe/zad1 sortowanie babelkowe/zad1 sortowanie babelkowe.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+void babelkowe_for(int tab[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (tab[j] > tab[j + 1]) {
+                int temp = tab[j];
+                tab[j] = tab[j + 1];
+                tab[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int tab[] = { 5, 2, 9, 1, 5, 6 };
+    int n = 5;
+
+    babelkowe_for(tab, n);
+
+    for (int i = 0; i < n; i++)
+        cout << tab[i] << "\t";
+}
